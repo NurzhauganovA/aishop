e9d2805bb90ed383640cdc0002b0f4137282f1c6
a.nurzhauganov@ggloberce.capital <a.nurzhauganov@ggloberce.capital>
2025-12-27 12:16:19 +0500

    fixes

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='sduclubs.asia,www.sduclubs.asia,localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='sduclubs.asia,www.sduclubs.asia,138.68.68.205,localhost,127.0.0.1', cast=Csv())
 
 # Application definition
 INSTALLED_APPS = [
